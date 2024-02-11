c52cab71febfbb1819d0ca974ac1b9584fe5ddfc
adamd <adamd@LAPTOP-G3EPG8TM>
2024-02-11 22:14:43 +0800

    Activity 3

---
+++ src/activity/pkg3/Activity3.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package activity.pkg3;
+
+/**
+ *
+ * @author adamd
+ */
+public class Activity3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       double c = 21f;
+       double a = 1.8f;
+       double b = 32f;
+       
+       double result = c * a + b;
+       System.out.println(c*a+b);
+    }
+    
+}
